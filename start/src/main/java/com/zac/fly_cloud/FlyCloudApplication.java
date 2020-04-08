0ca4768c0866c3989b9c4e2621858572052887aa
jinqiang.ning <jinqiang.ning@qiaofang.cn>
2020-04-08 18:17:30 +0800

    init

---
+++ start/src/main/java/com/zac/fly_cloud/FlyCloudApplication.java
@@ -0,0 +1,13 @@
+package com.zac.fly_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyCloudApplication.class, args);
+	}
+
+}
---
+++ start/src/main/java/test/java/com/zac/fly_cloud/FlyCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.zac.fly_cloud;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
