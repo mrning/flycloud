053c9aaff982444df10e6027d407ff830f436f86
zac <417298302@qq.com>
2020-09-16 22:50:00 +0800

    添加token

--- security/src/main/java/com/zac/flycloud/interceptor/handler/SecurityLoginSuccessHandler.java
+++ security/src/main/java/com/zac/flycloud/interceptor/handler/SecurityLoginSuccessHandler.java
@@ -2,12 +2,12 @@ package com.zac.flycloud.interceptor.handler;
 
 import com.alibaba.fastjson.JSONObject;
 import com.zac.flycloud.basebean.DataResponseResult;
+import com.zac.flycloud.entity.tablemodel.SysUser;
 import com.zac.flycloud.utils.TokenUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -29,7 +29,7 @@ public class SecurityLoginSuccessHandler extends SavedRequestAwareAuthentication
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
         log.info("SecurityLoginSuccessHandler  登录成功。");
-        String token = TokenUtil.createToken(((User)authentication.getPrincipal()).getUsername(),tokenKey);
+        String token = TokenUtil.createToken(((SysUser)authentication.getPrincipal()).getUsername(),tokenKey);
 
         response.setContentType("application/json; charset=UTF-8");
         response.getWriter().write(JSONObject.toJSONString(DataResponseResult.ok("登录成功",token)));
