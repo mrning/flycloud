9ad0571d808392b7c2207d764dbde696cbd2d938
zac <417298302@qq.com>
2021-05-23 20:42:14 +0800

    增加nacos + openfeign

--- start/src/main/java/com/zac/flycloud/FlyCloudApplication.java
+++ start/src/main/java/com/zac/flycloud/FlyCloudApplication.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.Environment;
 
@@ -12,6 +14,8 @@ import java.net.UnknownHostException;
 
 @Slf4j
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 @MapperScan(basePackages = {"com.zac.flycloud.mapper",
                             "com.zac.flycloud.actable.mapper",
                             "com.baomidou.mybatisplus.core.mapper"})
