e5c510caa0ad0b6a06bdde1cd1870d2e90ab345d
ning <417298302@qq.com>
2022-06-25 18:51:44 +0800

    build

--- flycloud-system-gateway/src/main/java/com/zac/flycloud/gateway/GatewayApplication.java
+++ flycloud-system-gateway/src/main/java/com/zac/flycloud/gateway/GatewayApplication.java
@@ -3,9 +3,13 @@ package com.zac.flycloud.gateway;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @Slf4j
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class GatewayApplication {
 
     public static void main(String[] args) {
